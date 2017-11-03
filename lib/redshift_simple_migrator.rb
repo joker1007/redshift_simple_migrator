7067ee4f44bceaa326f22aef8c9a4fb1805ba219
dingn1 <yanwding@gmail.com>
2017-11-03 11:54:07 -0400

    update

--- lib/redshift_simple_migrator.rb
+++ lib/redshift_simple_migrator.rb
@@ -21,6 +21,7 @@ require "redshift_simple_migrator/connection"
 require "redshift_simple_migrator/migrator"
 require "redshift_simple_migrator/migration"
 require "redshift_simple_migrator/cli"
+require "redshift_simple_migrator/tasks/redshift"
 
 if defined?(Rails)
   require "redshift_simple_migrator/railtie"
